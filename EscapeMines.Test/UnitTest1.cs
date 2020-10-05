3d661636648fac8437209832d89c3029df999b3d
ErinOsmani <erin.osmani19@gmail.com>
2020-10-05 22:20:48 +0200

    Init

---
+++ EscapeMines.Test/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace EscapeMines.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
